6fcdb4156f4fd921ed3934976d6ad9668e86280b
Snow-tear <snownamida@qq.com>
2020-08-04 16:06:14 +0800

    新增各种I/O函数整理列表

---
+++ 13 文件输入输出/读取文件并存储成字符串/main.c
@@ -0,0 +1,30 @@
+#include"main.h"
+
+int main(int argc, char* argv[]) {
+	FILE* fp;
+	register unsigned long int count = 1;
+	register char ch;
+	if (argc != 2) {
+		puts("ERROR: 请将文件拖入");
+		getchar();
+		exit(EXIT_FAILURE);
+	}
+	if ((fp = fopen(argv[1], "r")) == NULL) {
+		puts("ERROR: 未找到指定文件");
+		getchar();
+		exit(EXIT_FAILURE);
+	}
+	while ((ch = getc(fp)) != EOF)
+	{
+		count++;
+	}
+	char* string = calloc(count, sizeof(char));
+	fseek(fp, 0L, SEEK_SET);
+	for (count = 0; (ch = getc(fp)) != EOF; count++) {
+		string[count] = ch;
+	}
+	string[count + 1] = '\0';
+	printf("%s", string);
+	getchar();
+	exit(EXIT_FAILURE);
+}
